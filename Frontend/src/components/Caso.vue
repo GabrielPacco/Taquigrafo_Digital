3dd004d50e7d6baf31969debd44aa603b6011c1b
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-10-06 11:43:58 -0500

    Cambio de componentes

--- Frontend/src/components/Caso.vue
+++ Frontend/src/components/Caso.vue
@@ -1,207 +1,217 @@
 <template>
-    <v-app>
-      <v-main class="light-blue darken-4">
-      <v-container fill-height>
-    
-            <v-row
-              justify="center"
-              no-gutters
+  <v-app>
+    <v-main class="light-blue darken-4">
+    <v-container fill-height>
+  
+          <v-row
+            justify="center"
+            no-gutters
+          >
+            <v-dialog
+               max-width="700px"
             >
-              <v-dialog
-                 max-width="700px"
-              >
-                <template v-slot:activator="{ on, attrs }">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="white"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  AGREGAR UN NUEVO CASO
+                </v-btn>
+  
+              </template>
+              <v-card>
+                <br>
+                <v-card-title>
+                  <span class="text-h5 mx-auto">INGRESE LOS DATOS DEL NUEVO CASO</span>
+                </v-card-title>
+                <v-card-text class="text-h6 text-center">
+  
+                  <v-text-field
+                    class="mx-10"
+                    v-model='newTask2.Nombre'
+                    :rules="nameRules"
+                    label="Titulo"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    class="mx-10"
+                    v-model='newTask2.Descripcion'
+                    :rules="nameRules"
+                    label="Descripcion"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    class="mx-10"
+                    v-model='newTask2.Fecha'
+                    :rules="nameRules"
+                    label="Fecha de inicio"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    class="mx-10"
+                    v-model='newTask2.HoraIn'
+                    :rules="nameRules"
+                    label="Fecha de finalizacion"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    class="mx-10"
+                    v-model='newTask2.Estado'
+                    :rules="nameRules"
+                    label="Estado"
+                    required
+                  ></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
                   <v-btn
-                    color="white"
-                    v-bind="attrs"
-                    v-on="on"
+                    color="success"
+                    class="mr-4"
+                    @click="addCaso"
                   >
-                    AGREGAR UN NUEVO CASO
+                    CREAR CASO
                   </v-btn>
-    
-                </template>
-                <v-card>
-                  <br>
-                  <v-card-title>
-                    <span class="text-h5 mx-auto">INGRESE LOS DATOS DEL NUEVO CASO</span>
-                  </v-card-title>
-                  <v-card-text class="text-h6 text-center">
-    
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask2.Nombre'
-                      :rules="nameRules"
-                      label="Titulo"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask2.Descripcion'
-                      :rules="nameRules"
-                      label="Descripcion"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask2.Fecha'
-                      :rules="nameRules"
-                      label="Fecha de inicio"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask2.HoraIn'
-                      :rules="nameRules"
-                      label="Fecha de finalizacion"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask2.Estado'
-                      :rules="nameRules"
-                      label="Estado"
-                      required
-                    ></v-text-field>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="success"
-                      class="mr-4"
-                      @click="addTask"
-                    >
-                      CREAR CASO
-                    </v-btn>
-                  </v-card-actions>
-                  <br>
-                </v-card>
-              </v-dialog>
-              &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj;
-              <v-btn
-                color="white"
-                v-bind="attrs"
-                v-on="on"
-                link @click="$router.push({ path: '/concurso' })"
-              >
-                Configuracion
-              </v-btn>
-              &zwnj; &zwnj;
-            </v-row>
-    
-          <v-layout row wrap align>
-              <v-card
-              height="650" text-h4 class="mx-auto my-2 text-center" width="1600" outlined elevation="20">
-              <v-simple-table>
-                <template v-slot:default>
-                  <thead>
-                    <tr>
-                      <th class="text-center">
-                        Id
-                      </th>
-                      <th class="text-center">
-                        Titulo
-                      </th>
-                      <th class="text-center">
-                        Descripcion
-                      </th>
-                      <th class="text-center">
-                        Fecha de inicio
-                      </th>
-                      <th class="text-center">
-                        Fecha de finalizacion
-                      </th>
-                      <th class="text-center">
-                        Estado
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr
-                      v-for="task in tasks" :key="task"
-                    >
-                        <td>{{ task.id_act }} </td>
-                        <td>{{ task.nombre }} </td>
-                        <td>{{ task.descripcion }} </td>
-                        <td>{{ task.fecha }} </td>
-                        <td>{{ "-" }} </td>
-                        <td>{{ "Activo" }} </td>
-                        <td><v-btn
-                            small
-                            color="error"
-                            dark
-                            class="grey  mx-2"
-                            
-                            v-on="on"
-                            link @click="$router.push({ path: '/sesion' })"
-                          >
-                            <v-icon dark>
-                              mdi-lead-pencil
-                            </v-icon>
-                          </v-btn>
-    
-                          </td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
+                </v-card-actions>
+                <br>
               </v-card>
-            </v-layout>
-      </v-container>
-      </v-main>
-    </v-app>
-    </template>
-    
-    <script>
-    import axios from 'axios'
-    
-    export default{    
-        data(){ 
-            return { 
-                tasks: [],
-                newTask: {},
-                newTask2: {},
-                postURL: 'https://backendsecosystem.herokuapp.com',
-                config_request: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }       
-            }
-        },
-        methods:{
-            addTask(){ 
-                axios.post(this.postURL + '/actividad/add_actividad', this.newTask2, this.config_request)
-                    .then(res => {                                         
-                        this.tasks.push(res.data);
-                        console.log(res.data);
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });
-    
-                this.newTask2 = {};
-            },
-
-            deleteTask(task){                      
-                axios.post(this.postURL + '/actividad/delete_actividad', {CUI: task.ID_Actividad}, this.config_request)
-                    .then(() => {                      
-                        this.tasks.splice(this.tasks.indexOf(task), 1);                  
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });  
-            },
-            reset(){                      
-                this.newTask = {};
-            }
-    
-        },
-    
-        created(){ 
-            axios.post(this.postURL + '/actividad/get_actividades')
-                .then(res => { this.tasks = res.data; })
-                .catch((error) => { console.log(error) })
-        },
-    
-    }
-    </script>
+            </v-dialog>
+            &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj; &zwnj;
+            <v-btn
+              color="white"
+              v-bind="attrs"
+              v-on="on"
+              link @click="$router.push({ path: '/concurso' })"
+            >
+              Configuracion
+            </v-btn>
+            &zwnj; &zwnj;
+          </v-row>
+  
+        <v-layout row wrap align>
+            <v-card
+            height="650" text-h4 class="mx-auto my-2 text-center" width="1600" outlined elevation="20">
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-center">
+                      Id
+                    </th>
+                    <th class="text-center">
+                      Titulo
+                    </th>
+                    <th class="text-center">
+                      Descripcion
+                    </th>
+                    <th class="text-center">
+                      Fecha de inicio
+                    </th>
+                    <th class="text-center">
+                      Fecha de finalizacion
+                    </th>
+                    <th class="text-center">
+                      Estado
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="task in tasks" :key="task"
+                  >
+                      <td>{{ task.id_caso }} </td>
+                      <td>{{ task.titulo }} </td>
+                      <td>{{ task.descripcion }} </td>
+                      <td>{{ task.fecha_inicio }} </td>
+                      <td>{{ "-" }} </td>
+                      <td>{{ task.estado }} </td>
+                      <td><v-btn
+                          small
+                          color="error"
+                          dark
+                          class="grey  mx-2"
+                          
+                          v-on="on"
+                          @click="filterSesion(task.id_caso)"
+                        >
+                          <v-icon dark>
+                            mdi-lead-pencil
+                          </v-icon>
+                        </v-btn>
+  
+                        </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            </v-card>
+          </v-layout>
+    </v-container>
+    </v-main>
+  </v-app>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default{    
+      data(){ 
+          return { 
+              id_cliente: 0,
+              id_caso: 0,
+              tasks: [],
+              newTask: {},
+              newTask2: {},
+              postURL: 'https://backend-taquitranscript.herokuapp.com',
+              config_request: {
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*'
+              }       
+          }
+      },
+      methods:{
+          addCaso(){ 
+              axios.post(this.postURL + '/caso/add_caso', this.newTask2, this.config_request)
+                  .then(res => {                                         
+                      this.tasks.push(res.data);
+                      console.log(res.data);
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });
+  
+              this.newTask2 = {};
+          },
+          deleteCaso(task){                      
+              axios.post(this.postURL + '/caso/delete_caso', {CUI: task.ID_Actividad}, this.config_request)
+                  .then(() => {                      
+                      this.tasks.splice(this.tasks.indexOf(task), 1);                  
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });  
+          },
+          reset(){                      
+              this.newTask = {};
+          },
+          filterSesion(id_caso){
+            axios.post(this.postURL+ '/sesion/get_sesion_caso',{id_caso:id_caso},this.config_request)
+                .then(() => {
+                  this.id_caso=id_caso
+                  return this.$router.push({ path: '/sesion' })
+                })
+                .catch((error) => {
+                  console.log(error)
+                })
+          }
+      },
+  
+      created(){ 
+          axios.post(this.postURL + '/caso/get_casos')
+              .then(res => { this.tasks = res.data; })
+              .catch((error) => { console.log(error) })
+      },
+  
+  }
+  </script>
     
--- Frontend/src/components/Declaracion.vue
+++ Frontend/src/components/Declaracion.vue
@@ -1,194 +1,201 @@
 <template>
-    <v-app>
-      <v-main class="light-blue darken-4">
-      <v-container fill-height>
-        <v-row
-              align ="center"
-              justify="center"
-              no-gutters
+  <v-app>
+    <v-main class="light-blue darken-4">
+    <v-container fill-height>
+      <v-row
+        align ="center"
+        justify="center"
+        no-gutters
+      >
+        <v-dialog
+            max-width="700px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="white"
+              v-bind="attrs"
+              v-on="on"
             >
-              <v-dialog
-                 max-width="700px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="white"
-                    v-bind="attrs"
-                    v-on="on"
-                  >
-                    Agregar Nueva declaracion
-                  </v-btn>
-                  &zwnj; &zwnj;
-                </template>
-                <v-card>
-                  <br>
-                  <v-card-title>
-                    <span class="text-h5 mx-auto">Nueva Declaracion</span>
-                  </v-card-title>
-                  <v-card-text class="text-h6 text-center">
+              Agregar Nueva declaracion
+            </v-btn>
+            &zwnj; &zwnj;
+          </template>
+          <v-card>
+            <br>
+            <v-card-title>
+              <span class="text-h5 mx-auto">Nueva Declaracion</span>
+            </v-card-title>
+            <v-card-text class="text-h6 text-center">
 
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask.topico'
-                      :rules="nameRules"
-                      label="Tipo"
-                      required
-                    ></v-text-field>
+              <v-text-field
+                class="mx-10"
+                v-model='newTask.titulo'
+                :rules="nameRules"
+                label="Titulo"
+                required
+              ></v-text-field>
 
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask.topico'
-                      :rules="nameRules"
-                      label="Titulo"
-                      required
-                    ></v-text-field>
+              <v-text-field
+                class="mx-10"
+                v-model='newTask.tipo'
+                :rules="nameRules"
+                label="Tipo"
+                required
+              ></v-text-field>
 
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask.topico'
-                      :rules="nameRules"
-                      label="Sujeto"
-                      required
-                    ></v-text-field>
+              <v-file-input
+                counter
+                truncate-length="17"
+                class="mx-10"
+                v-model='newTask.audio'
+                :rules="nameRules"
+                label="Audio"
+                required
+              ></v-file-input>
 
-                    <v-text-field
-                      class="mx-10"
-                      v-model='newTask.topico'
-                      :rules="nameRules"
-                      label="Audio"
-                      required
-                    ></v-text-field>
-    
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="success"
-                      class="mr-4"
-                      @click="addTask"
-                    >
-                      AGREGAR
-                    </v-btn>
-                  </v-card-actions>
-                  <br>
-                </v-card>
-              </v-dialog>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
               <v-btn
-                color="white"
-                v-bind="attrs"
-                v-on="on"
-                link @click="$router.push({ path: '/admin' })"
+                color="success"
+                class="mr-4"
+                @click="addTask"
               >
-                Volver a actividades
+                AGREGAR
               </v-btn>
-            </v-row>
-    
-            <v-card
-              class="mx-auto"
-              max-width="1000"
-            >
-              <v-card-text>
-                <div>Tipo de declaracion</div>
-                <p class="text-h4 text--primary">
-                  Titulo
-                </p>
-                <p>Sujeto</p>
-                <div class="text--primary">
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.".
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                </div>
-              </v-card-text>
-            </v-card>
+            </v-card-actions>
+            <br>
+          </v-card>
+        </v-dialog>
+        <v-btn
+          color="white"
+          v-bind="attrs"
+          v-on="on"
+          link @click="$router.push({ path: '/admin' })"
+        >
+          Volver a actividades
+        </v-btn>
+      </v-row>
+  
+      <v-card
+        class="mx-auto"
+        width="1000"
+      >
+        <v-card-text>
+          <p class="text-h4 text--primary">
+            Titulo
+          </p>
+          <p>Tipo de declaracion</p>
+          <div class="text--primary">
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.".
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+          </div>
+        </v-card-text>
+      </v-card>
 
-            <v-card
-              class="mx-auto"
-              max-width="1000"
-            >
-              <v-card-text>
-                <div>Tipo de declaracion</div>
-                <p class="text-h4 text--primary">
-                  Titulo
-                </p>
-                <p>Sujeto</p>
-                <div class="text--primary">
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.".
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                  relating to or dependent on charity; charitable.
-                  "an eleemosynary educational institution.". 
-                </div>
-              </v-card-text>
-            </v-card>
+      <v-card
+        class="mx-auto"
+        width="1000"
+      >
+        <v-card-text>
+          <p class="text-h4 text--primary">
+            Titulo
+          </p>
+          <p>Tipo de declaracion</p>
+          <div class="text--primary">
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.".
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+            relating to or dependent on charity; charitable.
+            "an eleemosynary educational institution.". 
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-row v-for="task in tasks" :key="task">
+        <v-card
+        class="mx-auto"
+        width="1000"
+      >
+        <v-card-text>
+          <p class="text-h4 text--primary">
+            task.titulo
+          </p>
+          <p>task.tipo</p>
+          <div class="text--primary">
+            task.declaracion
+          </div>
+        </v-card-text>
+      </v-card>
+      </v-row>
+    </v-container>
+    </v-main>
+  </v-app>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default{    
+      data(){ 
+          return { 
+              tasks: [],
+              newTask: {},
+              postURL: 'https://backend-taquitranscript.herokuapp.com',
+              config_request: {
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*'
+              }       
+          }
+      },
+      methods:{
+          addTask(){ 
+              axios.post(this.postURL + '/declaracion/add_declaracion', this.newTask, this.config_request)
+                  .then(res => {                                         
+                      this.tasks.push(res.data);
+                      console.log(res.data)        ;
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });
+  
+              this.newTask = {};
+          },
+  
+          deleteTask(task){                      
+              axios.post(this.postURL + '/declaracion/delete_declaracion', {id_declaracion: task.id_declaracion}, this.config_request)
+                  .then(() => {                      
+                      this.tasks.splice(this.tasks.indexOf(task), 1);                    
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });  
+          },
+          reset(){                      
+              this.newTask = {};
+          }
+      },
       
-      </v-container>
-      </v-main>
-    </v-app>
-    </template>
-    
-    <script>
-    import axios from 'axios'
-    
-    export default{    
-        data(){ 
-            return { 
-                tasks: [],
-                newTask: {},
-                postURL: 'https://backendsecosystem.herokuapp.com',
-                config_request: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }       
-            }
-        },
-        methods:{
-            addTask(){ 
-                axios.post(this.postURL + '/ponencia/add_ponencia', this.newTask, this.config_request)
-                    .then(res => {                                         
-                        this.tasks.push(res.data);
-                        console.log(res.data)        ;
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });
-    
-                this.newTask = {};
-            },
-    
-            deleteTask(task){                      
-                axios.post(this.postURL + '/ponencia/delete_ponencia', {ID_Ponencia: task.id_pon}, this.config_request)
-                    .then(() => {                      
-                        this.tasks.splice(this.tasks.indexOf(task), 1);                    
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });  
-            },
-            reset(){                      
-                this.newTask = {};
-            }
-        },
-        
-        created(){ 
-            axios.post(this.postURL + '/ponencia/get_ponencias')
-                .then(res => { this.tasks = res.data; })
-                .catch((error) => { console.log(error) })
-        }
-    }
-    </script>
+      created(){ 
+          axios.post(this.postURL + '/declaracion/get_declaraciones')
+              .then(res => { this.tasks = res.data; })
+              .catch((error) => { console.log(error) })
+      }
+  }
+  </script>
